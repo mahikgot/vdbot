eb416abcdad9503fdf8447c2889353e3b2c3db01
Mark Guiang <mrguiang@pop-os.localdomain>
2020-07-07 01:45:51 +0800

    put on_message command implementation into on_message_mod.py file

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import discord
-import regex
-
+import on_message_mod
 client = discord.Client()
-
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -10,31 +8,12 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
-    async def hello():
-        await message.channel.send('Hello!', tts = True)
-    async def motivate():
-        if message.mention_everyone :
-            for i in message.guild.members :
-                await message.channel.send('You can do it <@' + str(i.id)  + '>!', tts = True) 
-            return
-            
-        hold = regex.findall(r'(?<=\<\@\!)(.*?)(?=\>)', message.content)
-        for i in hold :
-            await message.channel.send('You can do it <@' + i  + '>!', tts = True)
-#    async def play():
-#    hold = regex.findall(r'(?<=\<\@\!)(.*?)(?=\>)', message.content)
-    #insert new row into database containing user ids
-    #generate new group id number if have not played together before 
 
-    commands = {
-            '$hello' : hello,
-            '$motivate' : motivate,
-            }
 
     if message.author == client.user:
         return
-    if message.content.split(' ', 1)[0] in commands.keys() :
-        await commands[message.content.split(' ', 1)[0]]()
+    if message.content.split(' ', 1)[0] in on_message_mod.commands.keys() :
+        await on_message_mod.commands[message.content.split(' ', 1)[0]](message)
 
 
 

3c0c7e83197995f432dd22d8825f76b65f17bfbb
Mark Guiang <mrguiang@pop-os.localdomain>
2020-07-07 21:26:27 +0800

    create child of client object

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import discord
 import on_message_mod
-client = discord.Client()
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
+class Vbot(discord.Client):
+    commands = {
+            "$hello" : on_message_mod.hello,
+            "$motivate" : on_message_mod.motivate,
+            }
+    async def on_ready(self):
+        print('We have logged in as {0.user}'.format(self))
 
-
-
-    if message.author == client.user:
-        return
-    if message.content.split(' ', 1)[0] in on_message_mod.commands.keys() :
-        await on_message_mod.commands[message.content.split(' ', 1)[0]](message)
-
-
-
-client.run('NzI5Njg0OTI4MTk0Njc0NzU5.XwMjoA.tu0_LuzxcuiuwecBgtF5d5e46o4')
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.split(' ', 1)[0] in self.commands.keys() :
+            await self.commands[message.content.split(' ', 1)[0]](message)
+bot = Vbot()
+bot.run('NzI5Njg0OTI4MTk0Njc0NzU5.XwMjoA.tu0_LuzxcuiuwecBgtF5d5e46o4')
 
 
--- on_message_mod.py
+++ on_message_mod.py
@@ -1,20 +1,16 @@
 import regex
 
-commands = {
-        '$hello' : on_message_mod.hello,
-        '$motivate' : on_message_mod.motivate,
-        }
 def get_mention_id(text) :
    return regex.findall(r'(?<=\<\@\!)(.*?)(?=\>)', text)
 
 async def hello(message_obj):
-    await message_obj.channel.send('Hello!', tts = True)
+    await message_obj.channel.send('Hello!',)
 async def motivate(message_obj):
     if message_obj.mention_everyone :
         for i in message_obj.guild.members :
-            await message_obj.channel.send('You can do it <@' + str(i.id)  + '>!', tts = True) 
+            await message_obj.channel.send('You can do it <@' + str(i.id)  + '>!') 
         return
         
     hold = get_mention_id(message_obj.content)
     for i in hold :
-        await message_obj.channel.send('You can do it <@' + i  + '>!', tts = True)
+        await message_obj.channel.send('You can do it <@' + i  + '>!')
